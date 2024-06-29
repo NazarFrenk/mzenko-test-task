360cec736969a972cbd6d382944cf91441f64dae
nazarfrenk77 <nazarfrenk77@gmail.com>
2024-06-29 13:06:29 +0300

    Implement main formulas

--- CircuitComponentSolver.cpp
+++ CircuitComponentSolver.cpp
@@ -2,4 +2,59 @@
 
 CircuitComponentSolver::CircuitComponentSolver(QObject *parent)
     : QObject{parent}
-{}
+{
+    m_resistors = new std::deque<float>();
+}
+
+CircuitComponentSolver::~CircuitComponentSolver()
+{
+    delete m_resistors;
+}
+
+QString CircuitComponentSolver::calculateSeriesResistance()
+{
+    if (m_resistors->empty())
+    {
+        return "no data";
+    }
+
+    float total_resistance = 0;
+
+    for (float value: *m_resistors) {
+        total_resistance += value;
+    }
+
+    return QString("%1 Ω").arg(total_resistance, 0, 'f', 5);
+}
+
+QString CircuitComponentSolver::calculateParallelResistance()
+{
+    if (m_resistors->empty())
+    {
+        return "no data";
+    }
+
+    float total_resistance = 0;
+
+    for (float value: *m_resistors) {
+        total_resistance += 1/value;
+    }
+
+    total_resistance = 1/total_resistance;
+
+    return QString("%1 Ω").arg(total_resistance, 0, 'f', 5);
+}
+
+QString CircuitComponentSolver::calculateCRImpedance()
+{
+    float imaginary = 1 / (2 * M_PI * m_frequency * m_capacitor);
+
+    return QString("%1 + j%2 Ω").arg(m_resistor, 0, 'f', 5).arg(imaginary, 0, 'f', 5);
+}
+
+QString CircuitComponentSolver::calculateLRImpedance()
+{
+    float imaginary = 2 * M_PI * m_frequency * m_inductor;
+
+    return QString("%1 + j%2 Ω").arg(m_resistor, 0, 'f', 5).arg(imaginary, 0, 'f', 5);
+}
--- CircuitComponentSolver.h
+++ CircuitComponentSolver.h
@@ -2,14 +2,28 @@
 #define CIRCUITCOMPONENTSOLVER_H
 
 #include <QObject>
+#include <deque>
 
 class CircuitComponentSolver : public QObject
 {
     Q_OBJECT
 public:
     explicit CircuitComponentSolver(QObject *parent = nullptr);
+    ~CircuitComponentSolver();
 
 signals:
+
+private:
+    std::deque<float> *m_resistors = nullptr; // list of resistors for resistor tab
+    float m_resistor;
+    float m_capacitor;
+    float m_inductor;
+    float m_frequency;
+
+    QString calculateSeriesResistance();
+    QString calculateParallelResistance();
+    QString calculateCRImpedance();
+    QString calculateLRImpedance();
 };
 
 #endif // CIRCUITCOMPONENTSOLVER_H
